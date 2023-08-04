5c01158eca21a542ff6191d3e7e0b0ce4bec4835
yusaincedere <yusaincedere@gmail.com>
2023-08-04 16:12:53 +0300

    added comment

--- Solution.java
+++ Solution.java
@@ -1,8 +1,28 @@
+/**
+ *  Complete the function solveMeFirst to compute the sum of two integers.
+
+Example
+a = 7
+b = 3
+
+Return 10.
+
+Function Description
+
+Complete the solveMeFirst function in the editor below.
+
+solveMeFirst has the following parameters:
+
+int a: the first value
+int b: the second value
+Returns
+- int: the sum of a and b
+ */
+
 import java.util.*;
 
 public class Solution {
     private static Scanner scanner = new Scanner(System.in);
-    
     public static void main(String[] args) {
         int firstValue = readIntiger();
         int secondValue = readIntiger();
